bc6c0928727dce08629e8e8334a8d4eddfc2e77c
patatussss <98518531+Memoboy820@users.noreply.github.com>
2026-08-01 03:11:34 +0300

    Add files via upload

---
+++ BvvD/bot.py
@@ -0,0 +1,39 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+GUILD_ID = int(os.getenv("GUILD_ID"))
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents,
+    case_insensitive=True,
+    allowed_mentions=discord.AllowedMentions(
+        roles=True,
+        users=True,
+        replied_user=True
+    ),
+)
+
+async def setup_hook():
+    await bot.load_extension("cogs.YouTube")
+
+    guild = discord.Object(id=GUILD_ID)
+    bot.tree.copy_global_to(guild=guild)
+    synced = await bot.tree.sync(guild=guild)
+    print([cmd.name for cmd in synced])
+
+bot.setup_hook = setup_hook
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+bot.run(TOKEN)
