10003df997200922e4c64b6f849991ae4a082218
patatussss <98518531+Memoboy820@users.noreply.github.com>
2026-08-10 19:53:50 +0300

    Add 'Checker' extension to bot setup
    Added a new extension 'Checker' to the bot setup.

--- BvvD/bot.py
+++ BvvD/bot.py
@@ -25,6 +25,7 @@ bot = commands.Bot(
 async def setup_hook():
     await bot.load_extension("cogs.YouTube")
     await bot.load_extension("cogs.News")
+    await bot.load_extension("cogs.Checker")
 
     guild = discord.Object(id=GUILD_ID)
     bot.tree.copy_global_to(guild=guild)
