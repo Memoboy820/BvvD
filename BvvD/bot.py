c130920c20ce78265fd90b3f02f5c359d8ad3536
patatussss <98518531+Memoboy820@users.noreply.github.com>
2026-08-29 20:28:08 +0300

    Update bot.py

--- BvvD/bot.py
+++ BvvD/bot.py
@@ -48,6 +48,7 @@ async def setup_hook():
     await bot.load_extension("cogs.YouTube")
     await bot.load_extension("cogs.News")
     await bot.load_extension("cogs.Checker")
+    await bot.load_extension("cogs.Forum")
 
     guild = discord.Object(id=GUILD_ID)
 
