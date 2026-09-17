1bd2f276c2cc7479504cd7c6fbd75bf6c829fab8
patatussss <98518531+Memoboy820@users.noreply.github.com>
2026-09-17 22:50:29 +0300

    change in ai model

--- BvvD/cogs/Datamines.py
+++ BvvD/cogs/Datamines.py
@@ -346,7 +346,7 @@ class DataminesCog(commands.Cog):
 ## -- запрос на прогон и фильтрацию инфы через ии
 
             response = ai_client.models.generate_content(
-                model="gemini-3.8-flash",
+                model="gemini-3.5-flash-lite",
                 contents=f'{prompt}\nRaw Datamine Changes:\n{changed_files}'
             )
             ai_text_list = json.loads(response.text)
