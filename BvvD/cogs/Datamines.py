df0ef31d06ab11a5831adb7e0cc2976b559e7658
patatussss <98518531+Memoboy820@users.noreply.github.com>
2026-09-20 13:43:12 +0300

    Refactor DataminesCog for improved functionality

--- BvvD/cogs/Datamines.py
+++ BvvD/cogs/Datamines.py
@@ -372,11 +372,14 @@ class DataminesCog(commands.Cog):
             for guild_id, channel_id, role_id, last_datamine_sha in rows:
                 channel = self.bot.get_channel(channel_id)
                 if channel is not None:
-                    if 'No player-relevant datamine changes detected' in ai_text_list:
-                        await channel.send(content=f'<@&{role_id}>')
-                    for text in ai_text_list:
-                        await channel.send(content=f"# {message}: \n{text[:1950]}")
-                        await channel.send(content=f"**Data sourced from gszabi99's War Thunder Datamine repository** \n*📍Provided by BvvD bot*")
+                    try:
+                        if 'No player-relevant datamine changes detected' in ai_text_list:
+                            await channel.send(content=f'<@&{role_id}>')
+                        for text in ai_text_list:
+                            await channel.send(content=f"# {message}: \n{text[:1950]}")
+                            await channel.send(content=f"**Data sourced from gszabi99's War Thunder Datamine repository** \n*📍Provided by BvvD bot*")
+                    except Exception as e:
+                        print(f'[SEND] - {e}')
 
 # -- запись
                 conn = sqlite3.connect("/app/data/datamines.db")
